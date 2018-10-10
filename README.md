bf4a2425b27f0addc84ff39be31819787b67e54d
Verf <verf@protonmail.com>
2018-10-10 10:14:31 +0800

    v1.0

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # wubi-norman
 wubi(五笔) table for norman keyboard layout user
+基于norman键盘布局的fcitx格式的五笔86码表
+
+- qwe2nor.py: The Python script transform qwerty layout to norman layout
+- wbn.conf: wubi-norman config file
+- wbn.mb: wubi-norman table file
+- wbn.txt: wubi-norman txt format file
+- wbx.mb: default qwerty wubi table file
+- wbx.txt: default qwerty wubi txt format file
+
+### usage
+1. copy wbn.conf and wbn.mb file to ~/.config/fcitx/table/
+2. select wubin in fcitx configuration
+
+### qwe2nor usage
+Usage: qwd2nor.py <Source File> <Destination File>
+
+The source file must in txt format.
+You can transform mb format to txt format by mb2txt tool provided py fcitx.
+`mb2txt <mb file> > <txt file>`
+Also you can use txt2mb tool transform txt format to mb format.
+`txt2mb <txt file> <mb file>`
---
+++ qwe2nor.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+qwe2nor - transform qwerty layout table to norman layout
+
+Auther: Verf
+Email: verf@protonmail.com
+Licence: GPL v3
+"""
+import sys
+
+MAP = {'q': 'q',
+       'w': 'w',
+       'e': 'd',
+       'r': 'f',
+       't': 'k',
+       'y': 'j',
+       'u': 'u',
+       'i': 'r',
+       'o': 'l',
+       'p': 'h',
+       'a': 'a',
+       's': 's',
+       'd': 'e',
+       'f': 't',
+       'g': 'g',
+       'h': 'y',
+       'j': 'n',
+       'k': 'i',
+       'l': 'o',
+       'z': 'z',
+       'x': 'x',
+       'c': 'c',
+       'v': 'v',
+       'b': 'b',
+       'n': 'p',
+       'm': 'm', }
+
+
+def main():
+    if len(sys.argv) <= 1:
+        print("Error: No Argument!")
+        return
+    if sys.argv[1] == '-h':
+        print("Usage: qwd2nor.py <Source File> <Destination File>")
+        return
+    sour = sys.argv[1]
+    dest = sys.argv[2]
+    new_table = ''
+    with open(sour) as f:
+        isData = False
+        for line in f:
+            if '[Data]' in line:
+                isData = True
+            if isData:
+                new_line = ''
+                for char in line[:-1]:
+                    if char in MAP.keys():
+                        new_line += MAP[char]
+                    else:
+                        new_line += char
+                new_table += new_line + '\n'
+            else:
+                new_table += line
+    with open(dest, 'w') as f:
+        f.write(new_table)
+    print("Done!")
+
+
+if __name__ == '__main__':
+    main()
